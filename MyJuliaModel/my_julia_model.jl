d56f17fb27e346bfcee1ba2af7a05ef67047571f
Erkka Rinne <erkka.rinne@vtt.fi>
2020-10-05 13:38:23 +0300

    Add time dimension to Julia model

--- MyJuliaModel/my_julia_model.jl
+++ MyJuliaModel/my_julia_model.jl
@@ -1,7 +1,6 @@
 using SpineInterface
 using JuMP
 using Clp
-#using Plots
 
 db_url = ARGS[1]
 
@@ -10,17 +9,27 @@ using_spinedb(db_url)
 optimizer = optimizer_with_attributes(Clp.Optimizer)
 m = Model(optimizer)
 
-@variable(m, flow[(n, u) in node__unit()])
+# Get time indices
+time = sort(collect(keys(demand(node=node()[1]).mapping)))
+node__time = [(node=n, time=t) for n in node() for t in time]
+node__unit__time = [(node=n, unit=u, time=t) for (n, u) in node__unit() for t in time]
 
-@constraint(m, unit_capacity[(n, u) in node__unit()], 
-            flow[(node=n, unit=u)] <= capacity(unit=u))
+@variable(m, flow[[(node=n, unit=u, time=t) for (n, u, t) in node__unit__time]])
+
+@constraint(m, unit_capacity[(n, u, t) in node__unit__time], 
+            flow[(node=n, unit=u, time=t)] <= capacity(unit=u))
             
-@constraint(m, nodal_balance[n in node()],
-            sum(flow[(node=n, unit=u)] for u in node__unit(node=n)) == demand(node=n))
+@constraint(m, nodal_balance[(n, t) in node__time],
+            sum(flow[(node=n, unit=u, time=t)] 
+                for u in node__unit(node=n)) 
+            == demand(node=n, time=t))
 
 @objective(m, Min, 
-           sum(op_cost(unit=u) * flow[(node=n, unit=u)] for (n, u) in node__unit()))
+           sum(op_cost(unit=u) * flow[(node=n, unit=u, time=t)] 
+               for (n, u, t) in node__unit__time))
            
 optimize!(m)
 
-#bar(value.(flow).data, xticks=((1, 2), u.name for u in unit()), label="flow")
+# TODO
+#output_db_url = ARGS[2]
+#write_parameters(, output_db_url)
